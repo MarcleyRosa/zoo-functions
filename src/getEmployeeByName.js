e67c3dfc72d795859b96ba03086984b6ef7701fd
Marcley <marcleyrosa@gmail.com>
2022-06-30 16:39:58 -0300

    Requisito 3 e 4

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const test = employees.find((e) => e.firstName === employeeName || e.lastName === employeeName);
+  return test;
 }
+
+getEmployeeByName();
 
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((e) => e.managers.find((elem) => elem === id) === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  try {
+    const emp = employees.filter((e) => e.managers.find((el) => el === managerId));
+    return emp.map((e) => `${e.firstName} ${e.lastName}`);
+  } catch (error) {
+    return error.message;
+  }
 }
+getRelatedEmployees();
+isManager();
 
 module.exports = { isManager, getRelatedEmployees };

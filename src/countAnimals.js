1da14488671226c97a5d718ee0124fa013b26907
Marcley <marcleyrosa@gmail.com>
2022-06-30 21:29:15 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const objVaz = (acc, curr) => {
+  if (!acc[curr.name]) {
+    acc[curr.name] = curr.residents.length;
+  }
+  return acc;
+};
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce(objVaz, {});
+  }
+  if (animal.sex) {
+    return species.filter((e) => e.name === Object.values(animal)[0])
+      .map((e) => e.residents.filter((elem) => elem.sex === Object.values(animal)[1]))[0].length;
+  }
+  if (animal) {
+    return species.filter((e) => e.name === Object.values(animal)[0])
+      .map((e) => e.residents.length)[0];
+  }
 }
+
+countAnimals();
 
 module.exports = countAnimals;

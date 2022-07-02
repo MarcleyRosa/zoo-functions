462df574cb107fb2150a6cf07391bdbd10306cb5
Marcley <marcleyrosa@gmail.com>
2022-07-02 15:11:36 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const allIds = employees.filter((e) => e.id === id).map((e) => e.responsibleFor)[0][0];
+  const listAnimais = species.filter((e) => e.id === allIds)[0].residents;
+  const oldAge = listAnimais.map((e) => e.age);
+  const animalOld = listAnimais.find((e) => e.age === Math.max(...oldAge));
+  return Object.values(animalOld);
 }
+
+getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 
 module.exports = getOldestFromFirstSpecies;
